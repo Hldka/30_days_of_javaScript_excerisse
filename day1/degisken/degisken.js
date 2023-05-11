bc7c7cf4024d0ac7f71621c065c904dac0703c13
Hldka <116027001+Hldka@users.noreply.github.com>
2023-05-11 08:09:17 +0200

    d1

---
+++ day1/degisken/degisken.js
@@ -0,0 +1,27 @@
+let isim = "atom";
+let enKuckYapMi = true;
+let eletron;
+let nötron = null;
+console.log(typeof isim)
+console.log(typeof enKuckYapMi)
+console.log(typeof eletron)
+console.log(typeof nötron)
+
+let proton;
+let element;
+let molekul;
+let birlesik;
+proton =100;
+element =" 100"
+molekul =" yüz"
+birlesik = " yürü"
+
+let bilgi ={
+    ad : fizik,
+    soyad : lisans,
+    medeni : addEventListener,
+    ulke : uzay,
+    yas:infinity
+}
+console.log('ben 25 yasindayim')
+console.log('Sen 25 yasindayim')
---
+++ day2/style.js
@@ -0,0 +1,47 @@
+ let challenge =  '30 Days Of JavaScript';
+ console.log(challenge);
+ console.log(challenge.length);
+ challenge.toUpperCase;
+ challenge.toLowerCase;
+ challenge.substring(0,3)
+ challenge.substring(3)
+ console.log(challenge.includes('Script'));
+ challenge.split('')
+ challenge.split(' ')
+let challenge2 ='Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon' 
+
+challenge2.split(',')
+challenge2.replace('Script','Python');
+let ch = "JavaScript'in 30 Günü'";
+ch.charAt(15)
+ch.charCodeAt(0)
+challenge.indexOf('a')
+let dnm = 'Bir cümleyi çünkü ile bitiremezsiniz çünkü çünkü bir bağlaçtır';
+dnm.indexOf('cünkü')
+dnm.lastIndexOf('cünkü')
+challenge.trim
+challenge.startsWith('30')
+challenge.endsWith('Günü')
+challenge.match('a')
+let dnm1 = '30 Days of'
+let dnm2 = 'JavaScript'
+dnm1.concat(dnm2)
+challenge.repeat(1)
+
+console.log("The quote 'There is no exercise better for the heart than reaching down and lifting people up.' by John Holmes teaches us to help one another.")
+console.log("Love is not patronizing and charity isn't about pity, it is about love. Charity and love are the same -- with charity you give love, so don't just give money but reach out your hand instead.")
+console.log("10"==10)
+console.log("10"===10)
+let den = "python"
+let dan = "jargonda"
+dan.indexOf('on')
+den.indexOf('on')
+const num = Math.floor(Math.random () * 111) // 0 ile 100 arasında rastgele sayı oluşturur
+console.log(num)
+const num1 = Math.floor(Math.random () * 45) // 50 ile 100 arasında rastgele sayı oluşturur
+console.log(num)
+const num3 = Math.floor(Math.random () * 11) // 0 ile 255 arasında rastgele sayı oluşturur
+
+console.log(num)
+//10,11,12 1,2,3
+eval((5000*12)+10000+(15000*12))
